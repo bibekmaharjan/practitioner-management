34e1c609c87332da88e48850cfe7ba94336d03fe
Bibek Maharjan <bibekmaharjan12@gmail.com>
2023-05-23 12:35:11 +0545

    Refactor condition

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -8,9 +8,9 @@ import dayjs from 'dayjs';
  * @returns {string}
  */
 export const formatDate = (date: Date | string | undefined, formatting: string): string => {
-  if(!date) {
-    return "";
+  if (date) {
+    return dayjs(date).format(formatting);
   }
-  
-  return dayjs(date).format(formatting);
+
+  return '';
 };
