3d56de8c42476a63c3f68b86d908c6a40b918ba9
Bibek Maharjan <bibekmaharjan12@gmail.com>
2023-05-24 08:27:00 +0545

    Update test cases for interpolate util test

--- src/tests/utils/interpolate.spec.ts
+++ src/tests/utils/interpolate.spec.ts
@@ -2,33 +2,42 @@ import { interpolate } from "../../utils/interpolate";
 
 describe('interpolate', () => {
   it('should return the original string if params is null or empty', () => {
-    const str = 'Hello, {name}!';
+    const str = 'test.com/user/';
 
     expect(interpolate(str, null)).toBe(str);
     expect(interpolate(str, {})).toBe(str);
   });
 
   it('should interpolate placeholders with corresponding values', () => {
-    const str = 'Hello, {name}! You are {age} years old.';
-    const params = { name: 'Ramesh', age: 30 };
+    const str = 'test.com/user/{id}';
+    const params = { id: 7 };
 
-    const expected = 'Hello, Ramesh! You are 30 years old.';
+    const expected = 'test.com/user/7';
     expect(interpolate(str, params)).toBe(expected);
   });
+
+  it('should interpolate placeholders with multiple corresponding values', () => {
+    const str = 'test.com/user/{id}/practitioner/{practitionerId}';
+    const params = { id: 7, practitionerId: 37 };
+
+    const expected = 'test.com/user/7/practitioner/37';
+    expect(interpolate(str, params)).toBe(expected);
+  });
+
 
   it('should ignore null or undefined values in params', () => {
-    const str = 'Hello, {name}! You are {age} years old.';
-    const params = { name: 'Ramesh', age: null };
+    const str = 'test.com/user/{id}/practitioner/{practitionerId}';
+    const params = { id: null, practitionerId: 37 };
 
-    const expected = 'Hello, Ramesh! You are {age} years old.';
+    const expected = 'test.com/user/{id}/practitioner/37';
     expect(interpolate(str, params)).toBe(expected);
   });
 
-  it('should handle multiple occurrences of the same placeholder', () => {
-    const str = 'The value is {value}. {value} is important.';
-    const params = { value: '42' };
+  test('should not interpolate missing parameters', () => {
+    const str = 'test.com/user/{id}/practitioner/{practitionerId}';
+    const params = { id: 7 };
 
-    const expected = 'The value is 42. 42 is important.';
+    const expected = 'test.com/user/7/practitioner/{practitionerId}';
     expect(interpolate(str, params)).toBe(expected);
   });
 });
